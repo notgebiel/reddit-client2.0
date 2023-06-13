94e258922641d3ada7b2fb01df3b64ce46bd091a
notgebiel <cloostermansemile@gmail.com>
2023-06-13 09:09:56 +0200

    init commit

---
+++ src/components/searchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+
+
+class searchBar extends React.Component {
+    render() {
+        <p>searchbar</p>
+    }
+}
+
+const root = document.getElementById('')
+
